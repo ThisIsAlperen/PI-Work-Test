1231ab6ac7c11a238816f47fac9ded2c03203cbc
ThisIsAlperen <aydin.m.alperen@gmail.com>
2022-04-08 06:04:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -39,15 +39,32 @@ const APP = (function(Product ,UI){
             newProduct.push(subProduct)
             subProduct = [];
         });
-        
+        console.log(newProduct)
+        getTotal()
     }
     const findPrimes = function(e){
         e.forEach(number =>{
-            number = number*2;
-            subProduct.push(number)
+            sqrt = Math.floor(Math.sqrt(number));
+            
+            for(i = 2; i<sqrt+1;i++){
+                if(number % i == 0){
+                    subProduct.push(number)
+                }
+            }
+            if(number == 1){
+                subProduct.push(number)
+            }
         });
     }
+    const getTotal = function(){
+        const total = 0;
+        for(i=0;i<newProduct.length;i++){
+            initial = 0;
 
+            previous = initial;
+           
+        }
+    }
     return {
         init: function(){
             getProduct()
